06c6e2ebac9a625bb5f33d65e79d19e608879845
dleung87 <deniseleung@Denises-MacBook-Air.local>
2015-09-17 12:28:24 -0700

    added existing project

---
+++ skatepark/www/js/app.js
@@ -0,0 +1,61 @@
+(function(){
+  'use strict';
+  var module = angular.module('app', ['onsen']);
+
+  module.controller('AppController', function($scope, $data) {
+    $scope.doSomething = function() {
+      setTimeout(function() {
+        ons.notification.alert({ message: 'tapped' });
+      }, 100);
+    };
+  });
+
+  module.controller('DetailController', function($scope, $data) {
+    $scope.item = $data.selectedItem;
+      
+
+      
+  });
+
+  module.controller('MasterController', function($scope, $data) {
+    $scope.items = $data.items;
+
+    $scope.showDetail = function(index) {
+      var selectedItem = $data.items[index];
+      $data.selectedItem = selectedItem;
+      $scope.navi.pushPage('detail.html', {title : selectedItem.title});
+    };
+  });
+    
+    
+
+  module.factory('$data', function() {
+      var data = {};
+
+      data.items = [
+          {
+              title: 'Leeside',
+              label: '4h',
+              desc: 'RIP Lee.'
+          },
+          {
+              title: 'Burnside',
+              label: '6h',
+              desc: 'stoked!.'
+          },
+          {
+              title: 'China Creek',
+              label: '1day ago',
+              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+          },
+          {
+              title: 'Strathcona',
+              label: '1day ago',
+              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+          }
+      ];
+
+      return data;
+  });
+})();
+
